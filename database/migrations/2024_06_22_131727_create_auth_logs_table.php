ab4162e97a4fdcc3f225dec258ac5c9f50e3d4e5
amirhamzan <amirhamzan30@gmail.com>
2024-06-22 09:33:38 +0800

    Add feature AuthLog using event listeners

---
+++ database/migrations/2024_06_22_131727_create_auth_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auth_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('ip_address')->nullable();
+            $table->text('user_agent')->nullable();
+            $table->boolean('action'); // 1-login, 0-logout
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auth_logs');
+    }
+};
